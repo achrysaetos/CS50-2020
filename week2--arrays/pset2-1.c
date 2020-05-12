2c559bf74ba572a72fe968a4c0f0713010e3596d
achrysaetos <achrysaetos@yahoo.com>
2020-05-11 20:30:10 -0500

    week 2 lecture notes and pset done

---
+++ week2--arrays/pset2-1.c
@@ -0,0 +1,55 @@
+
+// Recommends reading level based on the Coleman-Liau index
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main(void){
+    printf("Text: ");
+    char text[1000];
+    fgets(text, sizeof(text), stdin);
+
+    int letters=0;
+    int spaces=0;
+    int sentences=0;
+    int misc=0;
+    for (int i=0; i<strlen(text); i++){
+        if(text[i]==' '){
+            spaces++;
+        }else if(text[i]=='.'||text[i]=='!'||text[i]=='?'){
+            sentences++;
+        }else if(text[i]==':'||text[i]==';'||text[i]=='-'||text[i]==','){
+            misc++;
+        }else{
+            letters++;
+        }
+    }
+
+    printf("Letters: ");
+    printf("%d\n", letters-1);
+    printf("Words: ");
+    printf("%d\n", spaces+1);
+    printf("Sentences: ");
+    printf("%d\n", sentences);
+    
+    int index = floor(.0588*(letters-1)/(spaces+1)*100 - .296*sentences/(spaces+1)*100 - 15.8);
+    printf("GRADE LEVEL: ");
+    printf("%d\n", index);
+
+}
+
+/*
+TEST CASES
+
+One fish. Two fish. Red fish. Blue fish. (Before Grade 1)
+Would you like them here or there? I would not like them here or there. I would not like them anywhere. (Grade 2)
+Congratulations! Today is your day. You're off to Great Places! You're off and away! (Grade 3)
+Harry Potter was a highly unusual boy in many ways. For one thing, he hated the summer holidays more than any other time of year. For another, he really wanted to do his homework, but was forced to do it in secret, in the dead of the night. And he also happened to be a wizard. (Grade 5)
+In my younger and more vulnerable years my father gave me some advice that I've been turning over in my mind ever since. (Grade 7)
+Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, "and what is the use of a book," thought Alice "without pictures or conversation?" (Grade 8)
+When he was nearly thirteen, my brother Jem got his arm badly broken at the elbow. When it healed, and Jem's fears of never being able to play football were assuaged, he was seldom self-conscious about his injury. His left arm was somewhat shorter than his right; when he stood or walked, the back of his hand was at right angles to his body, his thumb parallel to his thigh. (Grade 8)
+There are more things in Heaven and Earth, Horatio, than are dreamt of in your philosophy. (Grade 9)
+It was a bright cold day in April, and the clocks were striking thirteen. Winston Smith, his chin nuzzled into his breast in an effort to escape the vile wind, slipped quickly through the glass doors of Victory Mansions, though not quickly enough to prevent a swirl of gritty dust from entering along with him. (Grade 10)
+A large class of computational problems involve the determination of properties of graphs, digraphs, integers, arrays of integers, finite families of finite sets, boolean formulas and elements of other countable domains. (Grade 16+)
+
+*/
---
+++ week2--arrays/pset2-2.c
@@ -0,0 +1,73 @@
+
+//Encryption -- A maps to first letter of secret key, B maps to second letter, etc.
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    char secret[26] = 
+    //"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    "QWERTYUIOPASDFGHJKLZXCVBNM";
+    printf("plaintext: ");
+    char plaintext[1000];
+    fgets(plaintext, sizeof(plaintext), stdin);
+
+    printf("ciphertext: ");
+    for(int i=0; i<strlen(plaintext); i++){
+        if(plaintext[i]=='A'||plaintext[i]=='a'){
+            printf("%c", secret[0]);
+        }else if(plaintext[i]=='B'||plaintext[i]=='b'){
+            printf("%c", secret[1]);
+        }else if(plaintext[i]=='C'||plaintext[i]=='c'){
+            printf("%c", secret[2]);
+        }else if(plaintext[i]=='D'||plaintext[i]=='d'){
+            printf("%c", secret[3]);
+        }else if(plaintext[i]=='E'||plaintext[i]=='e'){
+            printf("%c", secret[4]);
+        }else if(plaintext[i]=='F'||plaintext[i]=='f'){
+            printf("%c", secret[5]);
+        }else if(plaintext[i]=='G'||plaintext[i]=='g'){
+            printf("%c", secret[6]);
+        }else if(plaintext[i]=='H'||plaintext[i]=='h'){
+            printf("%c", secret[7]);
+        }else if(plaintext[i]=='I'||plaintext[i]=='i'){
+            printf("%c", secret[8]);
+        }else if(plaintext[i]=='J'||plaintext[i]=='j'){
+            printf("%c", secret[9]);
+        }else if(plaintext[i]=='K'||plaintext[i]=='k'){
+            printf("%c", secret[10]);
+        }else if(plaintext[i]=='L'||plaintext[i]=='l'){
+            printf("%c", secret[11]);
+        }else if(plaintext[i]=='M'||plaintext[i]=='m'){
+            printf("%c", secret[12]);
+        }else if(plaintext[i]=='N'||plaintext[i]=='n'){
+            printf("%c", secret[13]);
+        }else if(plaintext[i]=='O'||plaintext[i]=='o'){
+            printf("%c", secret[14]);
+        }else if(plaintext[i]=='P'||plaintext[i]=='p'){
+            printf("%c", secret[15]);
+        }else if(plaintext[i]=='Q'||plaintext[i]=='q'){
+            printf("%c", secret[16]);
+        }else if(plaintext[i]=='R'||plaintext[i]=='r'){
+            printf("%c", secret[17]);
+        }else if(plaintext[i]=='S'||plaintext[i]=='s'){
+            printf("%c", secret[18]);
+        }else if(plaintext[i]=='T'||plaintext[i]=='t'){
+            printf("%c", secret[19]);
+        }else if(plaintext[i]=='U'||plaintext[i]=='u'){
+            printf("%c", secret[20]);
+        }else if(plaintext[i]=='V'||plaintext[i]=='v'){
+            printf("%c", secret[21]);
+        }else if(plaintext[i]=='W'||plaintext[i]=='w'){
+            printf("%c", secret[22]);
+        }else if(plaintext[i]=='X'||plaintext[i]=='x'){
+            printf("%c", secret[23]);
+        }else if(plaintext[i]=='Y'||plaintext[i]=='y'){
+            printf("%c", secret[24]);
+        }else if(plaintext[i]=='Z'||plaintext[i]=='z'){
+            printf("%c", secret[25]);
+        }else{
+            printf("%c", plaintext[i]);
+        }
+    }
+
+}
